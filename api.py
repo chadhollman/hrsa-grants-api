809f225a2c0ef7565de1e56f3b7e689792608b2e
leebaedke <leebaedke@gmail.com>
2024-11-12 21:39:26 -0500

    added a search route

--- api.py
+++ api.py
@@ -3,6 +3,73 @@ from flask_restful import Resource, Api
 import sqlite3
 import pandas as pd
 
+
+# United States of America Python Dictionary to translate States,
+
+
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "Virgin Islands, U.S.": "VI",
+}
+    
+# invert the dictionary
+abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -15,7 +82,7 @@ def get_db():
 @app.route('/name/<name>', methods=['GET'])
 def get_user_record(name):
     conn = get_db()
-    # traditional sql cursor not used in favor of a panda's dataframe for simplicity
+    # traditional sql cursor not used in favor of a pandas dataframe for simplicity
     # using the cursor would be more preformant
     cursor = conn.cursor()
     
@@ -54,11 +121,9 @@ def get_activity_code(code):
 @app.route('/topgrants/<topgrants>', methods=['GET'])
 def get_top_grants(topgrants):
     conn = get_db()
-    # traditional sql cursor not used in favor of a panda's dataframe for simplicity
-    # using the cursor would be more preformant
-    cursor = conn.cursor()
     
-    query = f"SELECT * FROM maternal_ehb_2023 WHERE State = '{topgrants}' ORDER BY Financial_Assistance DESC"
+    
+    query = f"SELECT * FROM maternal_ehb_2023 WHERE State = '{topgrants}'"
     
     df = pd.read_sql(query, conn)
     df = df[['Financial_Assistance', 'County', 'State', 'Grantee_Name', 'Program_Name']]
@@ -71,6 +136,30 @@ def get_top_grants(topgrants):
         })
     else:
         return jsonify({"error": "User record not found"}), 404
+    
+# take a Program_Name within a single state, and return a Program_Name s as well as total WIC participants within the same state
+@app.route('/program/<program>', methods=['GET'])
+def get_program(program):
+    conn = get_db()
+    
+    
+    query = f"SELECT * FROM combined_award_program_codes WHERE Grant_Program_Name = '{program}'"
+    
+    df = pd.read_sql(query, conn)
+    grant_code = df['Grant_Activity_Code'].iloc[0]
+    query = f"SELECT * FROM maternal_ehb_active WHERE Grant_Activity_Code = '{grant_code}'"
+    df = pd.read_sql(query, conn)
+
+    conn.close()
+    
+    # record = False
+    if len(df)!=0:
+        return jsonify({
+            "record": df.to_dict(orient = 'records')
+        })
+    else:
+        return jsonify({"error": "User record not found"}), 404
+
          
 # mimimal route example
 @app.route('/return/<name>')
